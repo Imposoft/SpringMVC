58763e6f223f177c8214446d7394f9307169f52b
apatagg <arturosdg@gmail.com>
2020-06-17 14:29:15 +0200

    Creado servicio REST en Spring

--- src/main/java/es/imposoft/SpringMVC/GreetingController.java
+++ src/main/java/es/imposoft/SpringMVC/GreetingController.java
@@ -5,43 +5,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.concurrent.atomic.AtomicLong;
 
-@Controller
+@RestController
 public class GreetingController {
 
-    @GetMapping(value = "/write")
-    public ResponseEntity<String> write(
-            @RequestParam(name="name", required=false, defaultValue="World") String name,
-            @RequestParam(name="msg", required=false, defaultValue="World") int msg
-            //Model model
-    ) {
-       // model.addAttribute("name", name);
-        //return ResponseEntity.ok(name);
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
 
-        try (FileOutputStream inputStream = new FileOutputStream(name)) {
-            inputStream.write(msg);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return ResponseEntity.ok(
-                msg + " escrito"
-        );
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
-
-    @GetMapping(value = "/read")
-    public ResponseEntity<String> read(
-            @RequestParam(name="name", required=false, defaultValue="World") String name
-            //Model model
-    ) {
-       // model.addAttribute("name", name);
-        //return ResponseEntity.ok(name);
-
-        return null;
-    }
-
 }
